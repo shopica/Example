e089c25de100522fb24126a7953b9ffaa9bc412b
Shopica <shopica18@gmail.com>
2023-10-31 16:38:02 -0700

    Add files via upload

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -1,14 +1,4 @@
 #Duplicate_example
-def calculate_area(length, width):
-    area = length * width
-    return area
-
-
-def calculate_perimeter(length, width):
-    perimeter = 2 * (length + width)
-    return perimeter
-
-
 def calculate_volume(length, width, height):
     volume = length * width * height
     return volume
