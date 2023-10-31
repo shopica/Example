6ce6aadddc3b9c6f2d6659854441cfb021f7e52d
Shopica <shopica18@gmail.com>
2023-10-31 16:31:47 -0700

    Add files via upload

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -2,9 +2,8 @@
 def calculate_total_price(items):
     total = 0
     for item in items:
-        if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+        if item.price > 100:
+            total += item.price * 0.9
+        else:
+            total += item.price * 0.95
     return total
